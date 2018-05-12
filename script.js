a6c4fe1f1f5f55afc1d68bc77ada94a9277cb27a
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-05-13 02:08:04 +0800

    idek

---
+++ script.js
@@ -0,0 +1,7 @@
+var x = ["https://media.giphy.com/media/Ojr9vupbS0v0GWDgxm/giphy.gif", "https://media2.giphy.com/media/bqbmKlYIA9474Hs9DX/giphy.gif"];
+var i = 0;
+
+function myFunction() {
+    document.getElementById("image").src = x[i];
+    i++;
+}

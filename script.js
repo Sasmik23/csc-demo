002ce05c60ca9cc3c74cde6556bb7d2ca1373635
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-05-14 09:45:41 +0800

    lol

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var x = ["https://media.giphy.com/media/Ojr9vupbS0v0GWDgxm/giphy.gif", "https://media2.giphy.com/media/bqbmKlYIA9474Hs9DX/giphy.gif"];
 var i = 0;
+var x = ["https://media0.giphy.com/media/l0Iy0aFeLUN9izAhG/giphy.gif","https://media.giphy.com/media/Ojr9vupbS0v0GWDgxm/giphy.gif"];
+
 
 function myFunction() {
     document.getElementById("image").src = x[i];
+    
     i++;
+
 }

e188311fca9b0aaea8fc19a0a14bbcae9d0ac985
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-07-08 23:26:07 +0800

    visitor count

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var i = 0;
-var x = ["https://media0.giphy.com/media/l0Iy0aFeLUN9izAhG/giphy.gif","https://media.giphy.com/media/Ojr9vupbS0v0GWDgxm/giphy.gif"];
+var x = ["https://media0.giphy.com/media/l0Iy0aFeLUN9izAhG/giphy.gif","https://media2.giphy.com/media/ulKtDV83bDAz5kcXAW/giphy.gifS"];
 
 
 function myFunction() {
     document.getElementById("image").src = x[i];
-    
-    i++;
+    i ++;
 
 }

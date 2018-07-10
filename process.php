357178c089c159a6707c13e98cad9ac3724bc503
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-07-10 09:45:18 +0800

    php

---
+++ process.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <?php
+        $name = $_POST["name"];
+        $class = $_POST["class"];
+        $feedback = $_POST["feedback"];
+
+        echo "Thank you ". $name . " for your feedback";
+
+
+        ?>
+    </body>
+</html>
